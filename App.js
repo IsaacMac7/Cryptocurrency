0e3ca045e22e7d5c144e9f226c8707e1dd2a90e8
IsaacMac <isaacmacandog7@gmail.com>
2022-01-13 21:12:07 +1100

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color:'white'}}>Hello world!</Text>
+      <Text style={{color:'white'}}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
